6a513d9b6c39f0370c9c0d0566ebade2602cdfe0
EmptyWalletException <1833865798@qq.com>
2020-02-05 21:31:49 +0800

    更新代码
    针对python3.8规范修正

--- bing.py
+++ bing.py
@@ -7,7 +7,7 @@ def url_open(url,accept):
     req = urllib.request.Request(url)
     header = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/18.17763'
     req.add_header('User-Agent',header)
-    if accept is 'true':   
+    if accept == 'true':   
         req.add_header('Accept', 'text/html, application/xhtml+xml, application/xml; q=0.9, */*; q=0.8')
     response = urllib.request.urlopen(req)
     html = response.read()
